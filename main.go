7fc85a610671818e6a840d47355140d1cc1c6ff2
Evan.Wang <evanwang.space@gmail.com>
2026-05-26 21:48:36 +0800

    gin 路由

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func pingHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong2",
+	})
+}
+
+
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello1",
+	})
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context){
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/ping", pingHandler)
+
+	router.GET("/Hello", helloHandler)
 
 	router.Run(":8080")
 }
