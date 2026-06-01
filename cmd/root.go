6add893bcf2b55fd7ab16989b7477229f9555f39
Evan.Wang <evanwang.space@gmail.com>
2026-06-01 22:35:57 +0800

    root.go 添加PersistentPreRunE 函数

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"GinAdmin/cmd/service"
 
@@ -16,6 +17,14 @@ var rootCmd = &cobra.Command{
 	Use: "gin-admin",
 	Short: "GinAdmin is a web framework for building RESTful APIs",
 	Long: "GinAdmin is a web framework for building RESTful APIss",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error { // 统一的初始化入口，避免每个子命令重复写启动代码。
+		// Cobra 命令的完整生命周期是：PersistentPreRunE → PreRunE → RunE → PostRunE → PersistentPostRunE   
+		
+		if shouldSkipBootstrap(cmd) {
+			return nil
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		err := service.RunServer()
 		if err != nil {
@@ -40,4 +49,26 @@ func init() {
 
 func registerCommands() {
 	rootCmd.AddCommand(service.ServeCmd)
+}
+
+
+/**
+* 判断是否跳过启动
+* 改函数如果返回 true ,则不用启动后续的日志等服务
+**/
+func shouldSkipBootstrap(cmd *cobra.Command) bool {
+	if cmd == nil {
+		return false
+	}
+	if cmd.Name() == "help" || cmd.Name() == "version" {
+		return true
+	}
+	commandPath := cmd.CommandPath()
+
+	switch commandPath {
+		case "gin-admin", "gin-admin version", "gin-admin help":
+			return true
+		default: 
+			return strings.HasPrefix(commandPath, "gin-admin completion") || strings.HasPrefix(commandPath, "gin-admin __complete")
+	}
 }
--- cmd/service/servece.go
+++ cmd/service/servece.go
@@ -36,12 +36,19 @@ func init() {
 	registerFlags()
 }
 
+
+/**
+* 注册命令行参数
+**/
 func registerFlags() {
 	ServeCmd.Flags().StringVarP(&host, "host", "H", defaultHost, "监听服务器地址")
 	ServeCmd.Flags().IntVarP(&port, "port", "P", defaultPort, "监听服务器端口")
 }
 
 
+/**
+* 启动服务器
+**/
 func RunServer() error {
 	engine, err := routers.SetRouters()
 	if err != nil {
@@ -66,61 +73,3 @@ func RunServer() error {
 
 	// return waitForShutdown(server, errChan)
 }
-
-
-
-// func waitForShutdown(server *http.Server, errChan <-chan error) error {
-// 	sigChan := make(chan os.Signal, 1)
-// 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-// 	defer signal.Stop(sigChan)
-
-// 	select {
-// 	case err, ok := <-errChan:
-// 		if ok && err != nil {
-// 			return err
-// 		}
-// 		return nil
-// 	case sig := <-sigChan:
-// 		log.Logger.Warn("Received API shutdown signal", zap.String("signal", sig.String()))
-// 	}
-
-// 	ctx, cancel := context.WithTimeout(context.Background(), gracefulShutdownTimout)
-// 	defer cancel()
-
-// 	if err := server.Shutdown(ctx); err != nil {
-// 		return fmt.Errorf("shutdown http server failed: %w", err)
-// 	}
-// 	if err := data.Shutdown(); err != nil {
-// 		return fmt.Errorf("shutdown data resources failed: %w", err)
-// 	}
-
-// 	log.Logger.Info("API service stopped gracefully")
-// 	return nil
-// }
-
-// package service
-
-// import (
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/spf13/cobra"
-// )
-
-// var serverCmd = &cobra.Command{
-// 	Use: "server",
-// 	Short: "Start the server",
-// 	Run: func(cmd *cobra.Command, args []string) {
-// 		r := gin.Default()
-
-// 		r.GET("/ping", func(c *gin.Context) {
-// 			c.JSON(200, gin.H{
-// 				"message": "pong",
-// 			})
-// 		})
-
-// 		r.Run(":8080")
-// 	},
-// }
-
-// func init() {
-// 	rootCmd.AddCommand(serverCmd)
-// }
