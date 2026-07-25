259bd3f86e1b3d830dc2e2e38cd1bc25a3ba3a79
Evan <evanwang.space@gmail.com>
2026-07-25 09:44:42 +0800

    Edit: 完善表哥你

--- web/src/pages/_layout/userList/index.tsx
+++ web/src/pages/_layout/userList/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Table, Form, Select, Input, Space, Button } from 'antd'
+import { Table, Form, Select, Input, Space, Button, Card } from 'antd'
 import { TableColumns } from './constant'
 import { FieldType } from './types'
 import { useUserList } from './hooks'
@@ -13,47 +13,55 @@ function RouteComponent() {
     const { form, pagination, USER_NAME_LIST_OPTIONS, user_list_data, handleSearch } = useUserList()
 
     return (
-        <Space orientation="vertical" size="medium" style={{ display: 'flex' }}>
-            <Form
-                form={form}
-                layout="inline"
-                wrapperCol={{ span: 8, style: { minWidth: '200px' } }}
-            >
-                <Form.Item<FieldType> label="姓名" name="nickname">
-                    <Select allowClear options={USER_NAME_LIST_OPTIONS} />
-                </Form.Item>
+        <Space orientation="vertical" size="medium" className="flex w-full">
+            <Card>
+                <Form
+                    form={form}
+                    layout="inline"
+                    wrapperCol={{ span: 8, style: { minWidth: '200px' } }}
+                >
+                    <Form.Item<FieldType> label="姓名" name="nickname">
+                        <Select allowClear options={USER_NAME_LIST_OPTIONS} />
+                    </Form.Item>
 
-                <Form.Item<FieldType> label="年龄" name="age">
-                    <Input />
-                </Form.Item>
-                <Form.Item>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        onClick={() => handleSearch({ currentPage: 1, pageSize: 10 })}
+                    <Form.Item<FieldType> label="年龄" name="age">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        wrapperCol={{ style: { minWidth: '0' } }}
+                        style={{ marginInlineStart: 'auto' }}
                     >
-                        Search
-                    </Button>
-                </Form.Item>
-            </Form>
-            <div className="my-12 bg-[#ccc]" />
-            <Table<UserListItemType>
-                bordered
-                columns={TableColumns}
-                dataSource={user_list_data}
-                rowKey="id"
-                pagination={{
-                    current: pagination.currentPage,
-                    pageSize: pagination.pageSize,
-                    total: pagination.total,
-                    onChange: (page, pageSize) => {
-                        handleSearch({
-                            currentPage: page,
-                            pageSize: pageSize,
-                        })
-                    },
-                }}
-            />
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            onClick={() => handleSearch({ currentPage: 1, pageSize: 10 })}
+                        >
+                            Search
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+            {/* <div className="my-12 bg-[#ccc]" /> */}
+            <Card>
+                <Table<UserListItemType>
+                    bordered
+                    columns={TableColumns}
+                    dataSource={user_list_data}
+                    rowKey="id"
+                    pagination={{
+                        current: pagination.currentPage,
+                        pageSize: pagination.pageSize,
+                        total: pagination.total,
+                        showSizeChanger: true,
+                        onChange: (page, pageSize) => {
+                            handleSearch({
+                                currentPage: page,
+                                pageSize: pageSize,
+                            })
+                        },
+                    }}
+                />
+            </Card>
         </Space>
     )
 }
