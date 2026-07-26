6158be6cf7b3814220719df4f2433e8d2a083ad5
Evan <evanwang.space@gmail.com>
2026-07-26 22:56:54 +0800

    添加业务错误特殊处理

---
+++ internal/service/admin_user_error.go
@@ -0,0 +1,17 @@
+package service
+
+import "GinAdmin/internal/pkg/errors"
+
+
+const (
+	CodeUsernameExists = 20001 // 用户名已存在
+	CodeUserInUse      = 20002 // 用户已绑定角色，不可删除
+	CodeUserNotFound   = 20003 // 用户不存在
+)
+
+
+var (
+	ErrUsernameExists = errors.NewBusinessError(CodeUsernameExists, "用户名已存在")
+	ErrUserInUse      = errors.NewBusinessError(CodeUserInUse, "用户已绑定角色，不可删除")
+	ErrUserNotFound   = errors.NewBusinessError(CodeUserNotFound, "用户不存在")
+)
